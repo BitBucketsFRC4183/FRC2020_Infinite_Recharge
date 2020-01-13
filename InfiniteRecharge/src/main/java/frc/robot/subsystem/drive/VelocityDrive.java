5a6e7aa757902b93085bae7100fb12a6d151ee65
Howard Beck <howard.beck314@gmail.com>
2020-01-13 12:49:50 -0700

    Make Drive Commands not use OI
    Not ready yet, still need to make a small change. But need to test something

--- InfiniteRecharge/src/main/java/frc/robot/subsystem/drive/VelocityDrive.java
+++ InfiniteRecharge/src/main/java/frc/robot/subsystem/drive/VelocityDrive.java
@@ -2,7 +2,6 @@ package frc.robot.subsystem.drive;
 
 
 
-import frc.robot.operatorinterface.OI;
 import frc.robot.utils.math.MathUtils;
 import frc.robot.utils.CommandUtils;
 
@@ -13,17 +12,15 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class VelocityDrive extends CommandBase {
     private final DriveSubsystem DRIVE_SUBSYSTEM;
-    private final OI OI;
 
 
 
     private final SlewRateLimiter SLEW_FILTER;
 
-    public VelocityDrive(DriveSubsystem driveSubsystem, OI oi) {
+    public VelocityDrive(DriveSubsystem driveSubsystem) {
         addRequirements(driveSubsystem);
 
         DRIVE_SUBSYSTEM = driveSubsystem;
-        OI = oi;
 
         double vel_ips = DRIVE_SUBSYSTEM.getSpeed_ips();
         SLEW_FILTER = new SlewRateLimiter(DriveConstants.MAX_LIN_ACCELERATION_IPSPS, vel_ips);
@@ -32,8 +29,8 @@ public class VelocityDrive extends CommandBase {
 
 
     public void execute() {
-        double rawSpeed = OI.speed();
-        double rawTurn = OI.turn();
+        double rawSpeed = DRIVE_SUBSYSTEM.getDriverRawSpeed();
+        double rawTurn = DRIVE_SUBSYSTEM.getDriverRawTurn();
 
         DRIVE_SUBSYSTEM.velocityDrive(rawSpeed, rawTurn);
 
@@ -52,15 +49,15 @@ public class VelocityDrive extends CommandBase {
     @Override
     public boolean isFinished() {
         if (DRIVE_SUBSYSTEM.getDriveMethod() == DriveSubsystem.DriveMethod.ROTATION) {
-            return CommandUtils.stateChange(new RotationDrive(DRIVE_SUBSYSTEM, OI));
+            return CommandUtils.stateChange(new RotationDrive(DRIVE_SUBSYSTEM));
         }
 
         if (DRIVE_SUBSYSTEM.getDriveMethod() == DriveSubsystem.DriveMethod.AUTO) {
-            return CommandUtils.stateChange(new AutoDrive(DRIVE_SUBSYSTEM, OI));
+            return CommandUtils.stateChange(new AutoDrive(DRIVE_SUBSYSTEM));
         }
 
         if (DRIVE_SUBSYSTEM.getDriveMethod() == DriveSubsystem.DriveMethod.IDLE) {
-            return CommandUtils.stateChange(new Idle(DRIVE_SUBSYSTEM, OI));
+            return CommandUtils.stateChange(new Idle(DRIVE_SUBSYSTEM));
         }
 
         return false;
