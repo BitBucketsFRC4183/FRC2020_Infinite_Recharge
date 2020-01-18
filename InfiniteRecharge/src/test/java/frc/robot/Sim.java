34f881357aa4cf70a7859641c6748d636de567be
Craig Post <cpost@bitbuckets.org>
2020-01-18 08:48:06 -0700

    Moved sim over to testImplementation dependency so it doesn't conflict with robot code Added templating to config.yaml to make changing sim ids easier Moved drive config ids into constants at the top of the file Added constants for drive motors to make changing them easier Instantiated feeder motor in ShooterSubsystem to fix NPE Commented out some Intake code because it's not ready yet.

---
+++ InfiniteRecharge/src/test/java/frc/robot/Sim.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+import java.io.IOException;
+
+import frc.robot.simulator.SimMain;
+
+public class Sim {
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        SimMain.main(args);
+    }
+}
