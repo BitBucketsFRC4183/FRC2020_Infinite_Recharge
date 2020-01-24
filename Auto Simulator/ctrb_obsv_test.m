879cb0a7a52d3537945e6809bde926a6bcd845c1
Howard Beck <howard.beck314@gmail.com>
2020-01-23 23:46:20 -0700

    Change physics of control system too

--- Auto Simulator/ctrb_obsv_test.m
+++ Auto Simulator/ctrb_obsv_test.m
@@ -1,33 +1,35 @@
-W = 26 + (11.25)/12;
-H = (52 + (5.25)/12);
-V_MAX = 16;
+W = 0.3048*(26 + (11.25)/12);
+H = 0.3048*(52 + (5.25)/12);
+V_MAX = 4.8768;
 
 nC = 0;
 nO = 0;
 n = 10000;
 
-C = eye(9);
+C = eye(3);
 
 for i = 1:n
     x = (2*rand - 1) * W;
     y = (2*rand - 1) * H;
-    v = (2*rand - 1) * V_MAX;
-    theta = 2*pi*rand;
-    omega = 2*pi*rand;
+    theta = (2*rand-1)*2*pi;
+    omegaL = (2*rand-1)*V_MAX;
+    omegaR = (2*rand-1)*V_MAX;
+    
+    u = [omegaL; omegaL; omegaR; omegaR];
     
     r = sqrt(x^2 + y^2);
     theta_r = atan2(y, x);
     
-    state = [r; theta_r; v; theta; omega];
+    state = [r; theta_r; theta;];
     
-    Ac = getSysMatControl(state);
+    Ac = getSysMatControl(state, u);
     Bc = getInpMatControl(state);
     
-    if (rank(ctrb(Ac, Bc)) == 9)
+    if (rank(ctrb(Ac, Bc)) == 3)
         nC = nC + 1;
     end
     
-    if (rank(obsv(Ac, C)) == 9)
+    if (rank(obsv(Ac, C)) == 3)
         nO = nO + 1;
     end
 end
--- Auto Simulator/getSysMatControl.m
+++ Auto Simulator/getSysMatControl.m
@@ -1,4 +1,4 @@
-function A = getSysMatControl(state)
+function A = getSysMatControl(state, u)
     controlConstants;
     
     A = zeros(STATE_SIZE, STATE_SIZE);
@@ -9,9 +9,9 @@ function A = getSysMatControl(state)
     % x'_v = cos theta0
     % x'_theta = -v0 sin(theta0)
     %
-    A(R, V) = cos(state(THETA) - state(THETA_R));
-    A(R, THETA) = state(V) * cos(state(THETA) - state(THETA_R));
-    A(R, THETA_R) = -state(V) * cos(state(THETA) - state(THETA_R));
+    v = WHEEL_RADIUS / 4 * (u(OMEGA_LT) + u(OMEGA_LB) + u(OMEGA_RT) + u(OMEGA_RB));
+    A(R, THETA) = v * cos(state(THETA) - state(THETA_R));
+    A(R, THETA_R) = -v * cos(state(THETA) - state(THETA_R));
     %A(X, V) = cos(state(THETA));
     %A(X, THETA) = -state(V) * sin(state(THETA));
     
@@ -21,19 +21,19 @@ function A = getSysMatControl(state)
     % y'_v = sin theta0
     % y'_theta = v0 cos(theta0)
     %
-    A(THETA_R, V) = sin(state(THETA) - state(THETA_R))/state(R);
-    A(THETA_R, R) = -state(V)/state(R)^2 * sin(state(THETA) - state(THETA_R));
-    A(THETA_R, THETA) = state(V)/state(R) * cos(state(THETA) - state(THETA_R));
-    A(THETA_R, THETA_R) = -state(V)/state(R) * cos(state(THETA) - state(THETA_R));
+    %A(THETA_R, V) = sin(state(THETA) - state(THETA_R))/state(R);
+    A(THETA_R, R) = -v/state(R)^2 * sin(state(THETA) - state(THETA_R));
+    A(THETA_R, THETA) = v/state(R) * cos(state(THETA) - state(THETA_R));
+    A(THETA_R, THETA_R) = -v/state(R) * cos(state(THETA) - state(THETA_R));
     %A(Y, V) = sin(state(THETA));
     %A(Y, THETA) = state(V) * cos(state(THETA));
     
-    A(THETA, OMEGA) = 1;
+    %A(THETA, OMEGA) = 1;
     
-    A(V, F_LT) = 1 / M;
-    A(V, F_LB) = 1 / M;
-    A(V, F_RT) = 1 / M;
-    A(V, F_RB) = 1 / M;
+    %A(V, F_LT) = 1 / M;
+    %A(V, F_LB) = 1 / M;
+    %A(V, F_RT) = 1 / M;
+    %A(V, F_RB) = 1 / M;
     
     %A(OMEGA, F_LT) = ROBOT_RADIUS / I_ROBOT;
     %A(OMEGA, F_LB) = ROBOT_RADIUS / I_ROBOT;
@@ -45,8 +45,8 @@ function A = getSysMatControl(state)
     %A(OMEGA_R, OMEGA_R) = W_W;
     %A(OMEGA_RB, OMEGA_RB) = W_W;
     
-    A(OMEGA, F_LT) = ROBOT_RADIUS / I_ROBOT;
-    A(OMEGA, F_LB) = ROBOT_RADIUS / I_ROBOT;
-    A(OMEGA, F_RT) = -ROBOT_RADIUS / I_ROBOT;
-    A(OMEGA, F_RB) = -ROBOT_RADIUS / I_ROBOT;
+    %A(OMEGA, F_LT) = ROBOT_RADIUS / I_ROBOT;
+    %A(OMEGA, F_LB) = ROBOT_RADIUS / I_ROBOT;
+    %A(OMEGA, F_RT) = -ROBOT_RADIUS / I_ROBOT;
+    %A(OMEGA, F_RB) = -ROBOT_RADIUS / I_ROBOT;
 end
