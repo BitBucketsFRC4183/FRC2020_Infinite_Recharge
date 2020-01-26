37e6d45364428f55c9167e498fbd4bdfa446ced0
Howard Beck <howard.beck314@gmail.com>
2020-01-25 18:26:01 -0700

    State-space code restructure and small changes
    Small changes
    - can't use motors 1-4 on JUNIOR for both shooting and driving so lets you switch between both now
    - add integrated encoder to Falcon shooter!
    - added Q and R matrices to simulator, so I can test LQR soon
    
    Big changes that haven't been tested but shouldn't affect anything not state-space-y
    - moved continuous to discrete converter to a more logical package and added some extra methods for ease of use
    - made ABFTriple and ABCouple, which let you store 3- and 2-tuples of matrices for state-space (x'=Ax+Bu+F)
    - all models are discrete by default, but have a continuous equivalent that converts to discrete and is a subclass
    - renamed from LTIK to LTIF. i honestly don't think either is right, since ppl only talk about LTI models but LTIF sounds better and works better with some ~math~ I've been doing
    - removed friction from motors for ease of implementation (for now, until I test all this)
    - LTI + F(t) model, where external "F"orces vary with time
    - Linearized model that all other LTIs are derived from, because nothing in life is linear except for lines themselves

---
+++ InfiniteRecharge/src/main/java/frc/robot/utils/control/statespace/models/ltif/CLTIFModel.java
@@ -0,0 +1,24 @@
+package frc.robot.utils.control.statespace.models.ltif;
+
+import org.ejml.simple.SimpleMatrix;
+
+import frc.robot.utils.control.statespace.models.ABFTriple;
+import frc.robot.utils.control.statespace.models.C2D;
+
+
+
+public class CLTIFModel extends LTIFModel {
+    protected final double DT;
+
+    public CLTIFModel(SimpleMatrix A, SimpleMatrix B, SimpleMatrix F, double dt) {
+        super(C2D.c2d(A, B, F, dt));
+
+        DT = dt;
+    }
+
+    public CLTIFModel(ABFTriple ABF, double dt) {
+        super(C2D.c2d(ABF, dt));
+
+        DT = dt;
+    }
+}
---
+++ InfiniteRecharge/src/main/java/frc/robot/utils/control/statespace/models/ltift/CLTIFtModel.java
@@ -0,0 +1,49 @@
+package frc.robot.utils.control.statespace.models.ltift;
+
+import org.ejml.simple.SimpleMatrix;
+
+import frc.robot.utils.control.statespace.models.ABCouple;
+import frc.robot.utils.control.statespace.models.C2D;
+
+public abstract class CLTIFtModel extends LTIFtModel {
+    private final double DT;
+
+
+
+    protected final SimpleMatrix GAMMA;
+
+
+
+    // Java bad
+    private CLTIFtModel(SimpleMatrix A, SimpleMatrix B, double dt, SimpleMatrix S, SimpleMatrix gamma) {
+        super(
+            C2D.getA(S, A.numRows()),
+            gamma.mult(B)
+        );
+
+        GAMMA = gamma;
+
+        DT = dt;
+    }
+
+    private CLTIFtModel(SimpleMatrix A, SimpleMatrix B, double dt, SimpleMatrix S) {
+        this(A, B, dt, S, C2D.getGamma(S, A.numRows()));
+    }
+
+    public CLTIFtModel(ABCouple AB, double dt) {
+        this(AB.getA(), AB.getB(), dt);
+    }
+
+    public CLTIFtModel(SimpleMatrix A, SimpleMatrix B, double dt) {
+        this(A, B, dt, C2D.getS(A, dt));
+    }
+
+
+    
+    @Override
+    protected SimpleMatrix updateF(double t) {
+        return GAMMA.mult(updateFc(t));
+    }
+
+    public abstract SimpleMatrix updateFc(double t);
+}
