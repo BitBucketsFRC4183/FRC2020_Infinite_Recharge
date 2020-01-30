15c3e34438e67ebf02ecebdc09aa824bbc1d6904
Howard Beck <howard.beck314@gmail.com>
2020-01-30 13:59:13 -0700

    Entire robot state system

--- InfiniteRecharge/src/main/java/frc/robot/subsystem/navigation/RobotSystem.java
+++ InfiniteRecharge/src/main/java/frc/robot/subsystem/navigation/RobotSystem.java
@@ -35,27 +35,43 @@ public class RobotSystem {
     }
 
     public CLTIFModel getModel() {
-        double t0 = System.nanoTime();
         double a = (1/M + R_R * R_R / I_R);
         double b = (1/M - R_R * R_R / I_R);
+        double c = 1.0/(2*R_R);
 
         double Cv = -(Kw * Kt * G * G * e / (R_R * R_W * R_W) + b * G * G * e / (R_W * R_W));
         double CV = G * e * Kt / (R_R * R_W);
-        double t1 = System.nanoTime();
-        //System.out.println("RobotSystem Setup time: " + (t1 - t0) / 1000000 + "ms");
 
-        SimpleMatrix Ac = new SimpleMatrix(2, 2, true, new double[] {a*Cv, b*Cv, b*Cv, a*Cv});
+        double theta0 = 2*Math.PI * Math.random();
+        double v0 = 15*(2 * Math.random() - 1);
 
-        SimpleMatrix Bc = new SimpleMatrix(2, 2, true, new double[] {a*CV, b*CV, b*CV, a*CV});
+        SimpleMatrix Ac = new SimpleMatrix(new double[][] {
+            new double[] {0, 0, -v0 * Math.sin(theta0), Math.cos(theta0) / 2, Math.cos(theta0) / 2},
+            new double[] {0, 0, v0 * Math.cos(theta0), Math.sin(theta0) / 2, Math.sin(theta0) / 2},
+            new double[] {0, 0, 0, c,    -c},
+            new double[] {0, 0, 0, a*Cv, b*Cv},
+            new double[] {0, 0, 0, b*Cv, a*Cv}
+        });
 
-        SimpleMatrix Fc = new SimpleMatrix(2, 1, true, new double[] {a*F_F_L + b*F_F_R, b*F_F_L + a*F_F_R});
+        SimpleMatrix Bc = new SimpleMatrix(new double[][] {
+            new double[] {0, 0},
+            new double[] {0, 0},
+            new double[] {0, 0},
+            new double[] {a*CV, b*CV},
+            new double[] {b*CV, a*CV}
+        });
 
-        double t2 = System.nanoTime();
+        SimpleMatrix Fc = new SimpleMatrix(new double[][] {
+            new double[] {v0 * theta0 * Math.sin(theta0)},
+            new double[] {-v0 * theta0 * Math.cos(theta0)},
+            new double[] {0},
+            new double[] {a*F_F_L + b*F_F_R},
+            new double[] {b*F_F_L + a*F_F_R}
+        });
+
         //System.out.println("RobotSystem continuous matrix time: " + (t2 - t1) / 1000000 + "ms");
 
         model = new CLTIFModel(Ac, Bc, Fc, 0.02);
-        double t3 = System.nanoTime();
-        //System.out.println("RobotSystem C2D time: " + (t3 - t2) / 1000000 + "ms");
 
         return model;
     }
