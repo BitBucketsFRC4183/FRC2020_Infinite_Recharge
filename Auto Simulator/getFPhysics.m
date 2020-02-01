bad470049ec78ea036bec9f645e3229a98127aa7
Howard Beck <howard.beck314@gmail.com>
2020-02-01 09:27:26 -0700

    Pole Placement controller tests
    and MATLAB got rid of an autosave file which is fine bc we don't care lol

--- Auto Simulator/getFPhysics.m
+++ Auto Simulator/getFPhysics.m
@@ -3,7 +3,7 @@ function F = getFPhysics(state, A)
     
     F = zeros(STATE_SIZE, 1);
     
-    v0 = (state(VL) + state(VR))/2;
+    v0 = (state(vL) + state(vR))/2;
     
     F(X) = v0 * state(THETA) * cos(state(THETA));
     F(Y) = -v0 * state(THETA) * sin(state(THETA));
