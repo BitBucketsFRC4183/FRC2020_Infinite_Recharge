46b63501e52e69f193dd2da0daecf646fdd58403
Elias Post Computer #1 <epost@bitbuckets.net>
2020-02-02 10:22:09 -0700

    Config changes and debug information.
    -Slightly changed junior config.
    
    -Slightly changed config.
    
    -Added soft limit functionality to motorConfig.
    
    -Added more debug info to the shooter.

--- InfiniteRecharge/src/main/java/frc/robot/utils/talonutils/MotorUtils.java
+++ InfiniteRecharge/src/main/java/frc/robot/utils/talonutils/MotorUtils.java
@@ -164,6 +164,9 @@ public class MotorUtils {
             break;
         }
 
+        motor.configMotionCruiseVelocity(motorConfig.motionMagicCruiseVelocity);
+        motor.configMotionAcceleration(motorConfig.motionMagicAcceleration);
+
         if (motorConfig.followingID != -1) {
             motor.set(ControlMode.Follower, motorConfig.followingID);
         }
