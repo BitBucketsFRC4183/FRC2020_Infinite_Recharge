cb6f8e7640be8a060cdd119375f351d1bcc89162
Elias Post Computer #3 <epost@bitbuckets.org>
2020-02-02 19:54:41 -0700

    Added elevation.
    -Added elevation, which is faster(not easier) said than done.
    
    -Changed filter length to one.
    
    -Added a "zero" button which makes the turret return to 0, 0. Currently square.
    
    -Changed azimuth PIDF.
    
    -Added motionMagicCruiseVelocity and motionMagicAcceleration to Config (they were in MotorConfig, but were never set in the main Config.)

--- InfiniteRecharge/src/main/java/frc/robot/subsystem/scoring/shooter/ShooterConstants.java
+++ InfiniteRecharge/src/main/java/frc/robot/subsystem/scoring/shooter/ShooterConstants.java
@@ -3,5 +3,5 @@ package frc.robot.subsystem.scoring.shooter;
 public class ShooterConstants {
     
     public static final boolean USE_FILTER = false;
-    public static final int FILTER_LENGTH = 25;
+    public static final int FILTER_LENGTH = 1;
 }
