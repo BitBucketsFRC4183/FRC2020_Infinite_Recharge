07b812d7c186c9c48f961bf1d57f2c33e4e52cb4
Sajo8 <andrewsplin@protonmail.com>
2020-02-02 20:08:03 -0700

    Add vision subsystem and add auto velocity (altitude) aiming

--- InfiniteRecharge/src/main/java/frc/robot/subsystem/scoring/shooter/ShooterConstants.java
+++ InfiniteRecharge/src/main/java/frc/robot/subsystem/scoring/shooter/ShooterConstants.java
@@ -4,4 +4,12 @@ public class ShooterConstants {
     
     public static final boolean USE_FILTER = false;
     public static final int FILTER_LENGTH = 25;
+
+    public static final double TARGET_HEIGHT_INCHES = 98.25;
+    public static final double CAMERA_HEIGHT_INCHES = 22; // approx (unknown for now)
+    public static final double CAMERA_MOUNTING_ANGLE = 30;
+    
+    public static final double BALL_SHOOTING_ANGLE = 30; // wild guess
+    public static final double SHOOTER_FLYWHEEL_RADIUS = 2; // inches
+
 }
