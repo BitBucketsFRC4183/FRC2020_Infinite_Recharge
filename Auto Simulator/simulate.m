08ff30267bf46a525c09327e2222807c7da5e30c
Howard Beck <howard.beck314@gmail.com>
2020-02-04 21:42:14 -0700

    Robot system Kalman filter
    Not fully implemented yet, just want to merge with BB master :)

--- Auto Simulator/simulate.m
+++ Auto Simulator/simulate.m
@@ -1,46 +1,67 @@
 physicsConstants;
 
 state = [
-    1; % x
-    5; % y
-    -pi-0.01; % heading, theta
-    0.01;
-    0.01;
+    0.0; % x
+    0.0; % y
+    0.0; % heading, theta
+    0;
+    0;
 ];
 
+state_hat = [5; 5; pi/3; 0; 0;];
 
-
-u = [0; 0;];
-T=0.02;
-
-i = 0;
-xs = zeros(1, 15/T + 1);
-ys = zeros(1, 15/T + 1);
-us = zeros(2, 15/T);
-
-xs2 = zeros(1, 15/T + 1);
-ys2 = zeros(1, 15/T + 1);
-
-
+P = 0.01*eye(5);
 
 Q = [
-    1/0.01^2, 0, 0, 0, 0;
-    0, 1/0.01^2, 0, 0, 0;
-    0, 0, 1/(1*pi/180)^2, 0, 0;
-    0, 0, 0, 1/(0.1)^2, 0;
-    0, 0, 0, 0, 1/(0.1)^2;
+    0.125, 0;
+    0, 0.125;
 ];
 
 R = [
-    1/12^2, 0;
-    0, 1/12^2;
+    0.125, 0, 0, 0;
+    0, pi/1800, 0, 0;
+    0, 0, 0.0001, 0; % pretty certain about velocities
+    0, 0, 0, 0.0001;
 ];
 
-state0 = [0; 0; 0; 0; 0;];
 
-states = zeros(5, 15/T + 1);
 
-for t=0:T:15
+u = [7; 10;];
+T=0.02;
+
+i = 0;
+
+TMAX = 1;
+
+xs = zeros(1, TMAX/T + 1);
+ys = zeros(1, TMAX/T + 1);
+xs_hat = zeros(1, TMAX/T + 1);
+ys_hat = zeros(1, TMAX/T + 1);
+us = zeros(2, TMAX/T);
+
+xs2 = zeros(1, TMAX/T + 1);
+ys2 = zeros(1, TMAX/T + 1);
+
+states = zeros(5, TMAX/T + 1);
+states_hat = zeros(5, TMAX/T + 1);
+
+es = zeros(1, TMAX/T + 1);
+
+
+kP = 2;
+kI = 0.0;
+kD = 0;
+kF = 2.551777109743181;
+
+lastVLerr = 0;
+lastVRerr = 0;
+
+vLfinal = 3;
+vRfinal = 5;
+
+
+
+for t=0:T:TMAX
     i = i + 1;
     
     xs(i) = state(X);
@@ -55,29 +76,69 @@ for t=0:T:15
     sysc = ss(Ac, Bc, eye(STATE_SIZE), zeros(STATE_SIZE, INPUT_SIZE));
     sysd = c2d(sysc, T);
     
+    %syscF = ss(Ac, Fc, eye(STATE_SIZE), zeros(STATE_SIZE, 1));
+    %sysdF = c2d(syscF, T);
+    
+    A = sysd.A;
+    B = sysd.B;
+    F = zeros(5, 1);%sysdF.B;
+    
+    lerr = state(vL) - vLfinal;
+    rerr = state(vR) - vRfinal;
+    
+    %u(1) = kF*vLfinal + kP*lerr;
+    %u(2) = kF*vRfinal + kP*rerr;
+    
+    u(1) = 0;
+    u(2) = 0;
+
+    %u(1) + 0.05*(2*rand-1);
+    %u(2) + 0.05*(2*rand-1);
+    
+    us(1:2, i) = u;
+    
+    % move state forward in time
+    %state = badUpdate(state, T);
+    state = A*state + B*u + F;
+    %state(THETA) = mod(state(THETA), 2*pi);
+    
+    states(1:5, i) = state;
+    
+    
+    
+    Ac = getSysMatPhysics(state_hat);
+    Bc = getInpMatPhysics(state_hat);
+    Fc = getFPhysics(state_hat);
+
+    sysc = ss(Ac, Bc, eye(STATE_SIZE), zeros(STATE_SIZE, INPUT_SIZE));
+    sysd = c2d(sysc, T);
+    
     syscF = ss(Ac, Fc, eye(STATE_SIZE), zeros(STATE_SIZE, 1));
     sysdF = c2d(syscF, T);
     
     A = sysd.A;
     B = sysd.B;
+    G = B;
     
-    P = dare(A, B, Q, R);
-    K = (R+B'*P*B)\B'*P'*A;
+    % predict
+    state_hat = A*state_hat + B*u + sysdF.B;
+    P = A*P*A' + G*Q*G';
     
-    u = -K*(state - state0) - pinv(sysd.B)*(sysdF.B + A*state0 - state0);
+    % measure
+    y = getOutputPhysics(state);
     
-    if (abs(u(1)) > 12)
-        u(1) = sign(u(1))*12;
-    end
+    % update
+    C = getCPhysics(state_hat);
+    K = P*C'/(C*P*C'+R);
+    state_hat = state_hat + K * (y - getOutputPhysics(state_hat)); 
+    P = (eye(5) - K*C)*P;
     
-    if (abs(u(2)) > 12)
-        u(2) = sign(u(2))*12;
-    end
+    xs_hat(i) = state_hat(1);
+    ys_hat(i) = state_hat(2);
     
-    us(1:2, i) = u;
+    states_hat(1:5, i) = state_hat;
     
-    state = A*state + B*u + sysdF.B;
-    states(1:5, i) = state;
+    es(i) = sqrt((state(X) - state_hat(X))^2 + (state(Y) - state_hat(Y))^2); 
 end
 
 FIELD_W = (26 + 11.25/12)*0.3048;
@@ -85,16 +146,26 @@ FIELD_H = (52 + 5.25/12)*0.3048;
 
 figure(1);
 plot(xs, ys);
+hold on;
+plot(xs_hat, ys_hat);
 axis([-FIELD_W/2, FIELD_W/2, -FIELD_H/2, FIELD_H/2]);
 daspect([1, 1, 1]);
+legend(["actual", "estimated"]);
+hold off;
 
 figure(2);
-plot(xs2, ys2);
-axis([-FIELD_W/2, FIELD_W/2, -FIELD_H/2, FIELD_H/2]);
-daspect([1, 1, 1]);
+plot(0:T:TMAX, states(vL:vR, 1:(1 + TMAX/T)));
+title("Velocity over time");
 
 figure(3);
-plot(0:T:15, us);
+plot(0:T:TMAX, states(THETA, 1:(1 + TMAX/T)));
+title("Heading over time");
 
-figure(4);
-plot(0:T:15, states(4:5, 1:(15/T + 1)));
+figure(5);
+plot(0:T:TMAX, us);
+
+figure(6);
+plot(0:T:TMAX, states(4:5, 1:(TMAX/T + 1)));
+
+figure(7);
+plot(0:T:TMAX, es*12/0.3048);
