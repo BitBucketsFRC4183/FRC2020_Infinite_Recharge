c6f229103a0913a3881a5244ec70c308163f6838
Howard Beck <howard.beck314@gmail.com>
2020-02-16 15:43:46 -0700

    Fixed independent drive and rotation

--- InfiniteRecharge/src/main/java/frc/robot/subsystem/pidhelper/PIDHelperSubsystem.java
+++ InfiniteRecharge/src/main/java/frc/robot/subsystem/pidhelper/PIDHelperSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystem.pidhelper;
 
 import com.ctre.phoenix.ParamEnum;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.BaseTalon;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
@@ -9,8 +10,10 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.config.Config;
+import frc.robot.config.MotorConfig;
 import frc.robot.config.MotorConfig.EncoderType;
 import frc.robot.subsystem.BitBucketSubsystem;
+import frc.robot.utils.control.pidf.PIDF;
 import frc.robot.utils.talonutils.MotorUtils;
 
 
@@ -66,6 +69,17 @@ public class PIDHelperSubsystem extends BitBucketSubsystem {
     private final String MM_ACC = getName() + "/MM acceleration";
     private final String MM_CRUISE = getName() + "/MM cruise velocity";
 
+    private final String POSITION = getName() + "/Encoder position";
+    private final String VELOCITY = getName() + "/Encoder velocity";
+    private final String CLOSED_LOOP_ERROR = getName() + "/Closed loop error";
+
+    private final String CONTROL_MODE = getName() + "/Control Mode";
+    private final SendableChooser<ControlMode> CONTROL_MODE_CHOOSER;
+
+    private final String INPUT = getName() + "/Input";
+
+    private final String UPDATE = getName() + "/Update config";
+
     private BaseTalon talon = new WPI_TalonSRX(0);
 
     private int lastTalonID = 0;
@@ -82,6 +96,8 @@ public class PIDHelperSubsystem extends BitBucketSubsystem {
         controllerChooser = getControllerChooser(Controllers.NONE);
         slotChooser = getSlotChooser(Slots.POSITION);
         encoderChooser = getEncoderTypeChooser(EncoderType.None);
+
+        CONTROL_MODE_CHOOSER = new SendableChooser<ControlMode>();
     }
 
 
@@ -104,6 +120,11 @@ public class PIDHelperSubsystem extends BitBucketSubsystem {
         SmartDashboard.putBoolean(INVERTED, false);
         SmartDashboard.putNumber(MM_ACC, 0);
         SmartDashboard.putNumber(MM_CRUISE, 0);
+
+        CONTROL_MODE_CHOOSER.setDefaultOption("PercentOutput", ControlMode.PercentOutput);
+        CONTROL_MODE_CHOOSER.addOption("Velocity", ControlMode.Velocity);
+        CONTROL_MODE_CHOOSER.addOption("MotionMagic", ControlMode.MotionMagic);
+        SmartDashboard.putData(CONTROL_MODE, CONTROL_MODE_CHOOSER);
     }
 
 	public void testInit() {
@@ -117,7 +138,17 @@ public class PIDHelperSubsystem extends BitBucketSubsystem {
             lastTalonID = id;
 
             updateMotor();
+
+            SmartDashboard.putNumber(INPUT, 0);
         }
+
+        if (SmartDashboard.getBoolean(UPDATE, false)) {
+            update();
+            
+            SmartDashboard.putBoolean(UPDATE, false);
+        }
+
+        talon.set(CONTROL_MODE_CHOOSER.getSelected(), SmartDashboard.getNumber(INPUT, 0));
     }
 	
 	public void diagnosticsCheck() {
@@ -186,8 +217,38 @@ public class PIDHelperSubsystem extends BitBucketSubsystem {
         SmartDashboard.putNumber(I_ZONE, talon.configGetParameter(ParamEnum.eProfileParamSlot_IZone, slot));
     }
 
+    private void update() {
+        MotorConfig config = new MotorConfig();
+        
+        config.encoderType = encoderChooser.getSelected();
+        config.inverted = SmartDashboard.getBoolean(INVERTED, false);
+        config.motionMagicAcceleration = (int) SmartDashboard.getNumber(MM_ACC, 0);
+        config.motionMagicCruiseVelocity = (int) SmartDashboard.getNumber(MM_CRUISE, 0);
+        config.positionPIDF = new PIDF(
+            talon.configGetParameter(ParamEnum.eProfileParamSlot_P, MotorUtils.positionSlot),
+            talon.configGetParameter(ParamEnum.eProfileParamSlot_I, MotorUtils.positionSlot),
+            talon.configGetParameter(ParamEnum.eProfileParamSlot_D, MotorUtils.positionSlot),
+            talon.configGetParameter(ParamEnum.eProfileParamSlot_F, MotorUtils.positionSlot)
+        );
+        config.velocityPIDF = new PIDF(
+            talon.configGetParameter(ParamEnum.eProfileParamSlot_P, MotorUtils.velocitySlot),
+            talon.configGetParameter(ParamEnum.eProfileParamSlot_I, MotorUtils.velocitySlot),
+            talon.configGetParameter(ParamEnum.eProfileParamSlot_D, MotorUtils.velocitySlot),
+            talon.configGetParameter(ParamEnum.eProfileParamSlot_F, MotorUtils.velocitySlot)
+        );
+        config.id = lastTalonID;
+
+
+
+        MotorUtils.motorInit(talon, config);
+    }
+
     @Override
-    public void dashboardPeriodic(float deltaTime) {}
+    public void dashboardPeriodic(float deltaTime) {
+        SmartDashboard.putNumber(POSITION, talon.getSelectedSensorPosition());
+        SmartDashboard.putNumber(VELOCITY, talon.getSelectedSensorVelocity());
+        SmartDashboard.putNumber(CLOSED_LOOP_ERROR, talon.getClosedLoopError());
+    }
 
 
 
