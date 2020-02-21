87fbc6dd0878507e8c5cd674d83aeee66387b506
Howard Beck <howard.beck314@gmail.com>
2020-02-20 21:49:37 -0700

    Debugging UKF
    yay she works now

--- Auto Simulator/ukf_test.m
+++ Auto Simulator/ukf_test.m
@@ -1,6 +1,6 @@
 f = @(x) [x(1).^2 + abs(x(2)); sin(x(2)) - sqrt(x(1))];
 
-h = @(x) [tan(x(1)); 1/x(2)];
+h = @(x) [tan(x(1) + x(2)); 1/x(2)];
 
 Q = [
     3, 2;
@@ -18,8 +18,16 @@ P = [
 ];
 
 x_hat = [1; 2;];
-x = x_hat + mvnrnd([0; 0;], P, 1)';
+x = x_hat; % for simplicity
 
-x = f(x) + mvnrnd([0; 0;], Q, 1)';
-y = h(x) + mvnrnd([0; 0;], R, 1)';
+x = f(x);
+y = [-0.23; -11];%h(x) + mvnrnd([0; 0;], R, 1)';
 [x_hat, P] = ukf(f, x_hat, P, h, y, Q, R);
+
+x_hat
+
+x = f(x);
+y = [pi/2; -0.54];
+[x_hat, P] = ukf(f, x_hat, P, h, y, Q, R);
+
+x_hat
