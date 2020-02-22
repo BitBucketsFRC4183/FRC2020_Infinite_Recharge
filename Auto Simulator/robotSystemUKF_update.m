09454138d8d83a3858869b82801fb42fa627bed3
Howard Beck <howard.beck314@gmail.com>
2020-02-22 00:40:22 -0700

    Initial tests with better model of system

---
+++ Auto Simulator/robotSystemUKF_update.m
@@ -0,0 +1,9 @@
+function x3 = robotSystemUKF_update(f, ts, x1, u)
+    physicsConstants;
+    
+    f_u = @(T, X) f(X, u);
+    [~, x2] = ode45(f_u, ts, x1);
+    [steps, ~] = size(x2);
+    x3 = x2(steps, :)';
+    x3(THETA) = mod(x3(THETA) + pi, 2*pi) - pi;
+end
