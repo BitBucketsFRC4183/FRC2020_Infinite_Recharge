818aa26e08740d70abd85bcf9fc99718107a2e2d
Howard Beck <howard.beck314@gmail.com>
2020-02-23 09:02:11 -0700

    Relocated SabaMath and implemented single output spline class
    this Spline is more general than Saba's spline, hopefully one day I can consolidate both pieces of code

--- InfiniteRecharge/src/main/java/frc/robot/utils/math/interpolation/spline/parametric/SabaMath.java
+++ InfiniteRecharge/src/main/java/frc/robot/utils/math/interpolation/spline/parametric/SabaMath.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.utils.math.interpolation.spline;
+package frc.robot.utils.math.interpolation.spline.parametric;
 
 //the closest I will get to credibility in mathematics
 public class SabaMath 
