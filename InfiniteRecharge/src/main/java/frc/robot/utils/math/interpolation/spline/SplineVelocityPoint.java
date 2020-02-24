be61557e5601de1f866c95348550fe40a58bdb0a
Ojas Sanghi <ojas.sanghi@protonmail.com>
2020-02-23 19:57:58 -0700

    add spline class which takes in ArrayList<VelocityPoint>
    merge in/use after my shooter_stuff branch has been merged in

---
+++ InfiniteRecharge/src/main/java/frc/robot/utils/math/interpolation/spline/SplineVelocityPoint.java
@@ -0,0 +1,32 @@
+package frc.robot.utils.math.interpolation.spline;
+
+import java.util.ArrayList;
+import frc.robot.subsystem.scoring.shooter.ShooterCalculator.VelocityPoint;
+
+public class SplineVelocityPoint {
+    private final Spline speedSpline;
+    private final Spline angleSpline;
+
+    public SplineVelocityPoint(ArrayList<VelocityPoint> vPoints) {
+        double[] distanceX =  new double[vPoints.size()];
+        double[] speedY = new double[vPoints.size()];
+        double[] angleY = new double[vPoints.size()];
+
+        for (int i = 0; i < vPoints.size(); i++) {
+            distanceX[i] = vPoints.get(i).getDistance_in();
+            speedY[i] = vPoints.get(i).getSpeed_rpm();
+            angleY[i] = vPoints.get(i).getHoodAngle_deg();
+        }
+
+        speedSpline = new Spline(distanceX, speedY);
+        angleSpline = new Spline(distanceX, angleY);
+    }
+
+    public double getSpeedSpline(double x) {
+        return speedSpline.get(x);
+    }
+
+    public double getAngleSpline(double x) {
+        return angleSpline.get(x);
+    }
+}
