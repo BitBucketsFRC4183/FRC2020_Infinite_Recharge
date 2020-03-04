5906de0f43584a950cd5f4157569f1377e8c34e4
Sajo8 <andrewsplin@protonmail.com>
2020-03-03 17:33:44 -0700

    take into account zoom and pan
    i'd like to cycle thru pipelines if i can't find a target ideally so need to figure that out

--- InfiniteRecharge/src/main/java/frc/robot/subsystem/vision/VisionSubsystem.java
+++ InfiniteRecharge/src/main/java/frc/robot/subsystem/vision/VisionSubsystem.java
@@ -23,6 +23,8 @@ public class VisionSubsystem extends BitBucketSubsystem {
     private double ty = 0;
 
     private double distance = 0;
+    private double zoom = 1;
+    private double pan = 0;
 
     public VisionSubsystem(final Config config) {
         super(config);
@@ -54,15 +56,16 @@ public class VisionSubsystem extends BitBucketSubsystem {
 
         updateTargetInfo();
         distance = approximateDistanceFromTarget(ty);
-        // adjustZoom();
+        adjustZoom();
 
         SmartDashboard.putBoolean(getName() + "/Valid Target ", validTarget);
         SmartDashboard.putNumber(getName() + "/Estimated Distance ", distance);
     }
 
     public double approximateDistanceFromTarget(final double ty) {
-        return (VisionConstants.TARGET_HEIGHT_INCHES - VisionConstants.CAMERA_HEIGHT_INCHES)
+        double distance_no_zoom = (VisionConstants.TARGET_HEIGHT_INCHES - VisionConstants.CAMERA_HEIGHT_INCHES)
                 / Math.tan(Math.toRadians(VisionConstants.CAMERA_MOUNTING_ANGLE + ty));
+        return distance_no_zoom * zoom;
     }
 
     public double queryLimelightNetworkTable(final String value) {
@@ -80,24 +83,41 @@ public class VisionSubsystem extends BitBucketSubsystem {
 
         tx = queryLimelightNetworkTable("tx");
         ty = queryLimelightNetworkTable("ty");
+        ty -= pan;
     }
 
     public void adjustZoom() {
         double pipelineToChangeTo = 0;
 
-        // TODO: empirically test this
-        // higher zoom (higher pipeline) the further u go
+        //TODO: empirically test this
+        //higher zoom (higher pipeline) the further u go
         if (distance >= 0) {
             pipelineToChangeTo = 0;
         }
-        if (distance >= 10) {
+        if (distance >= 200) {
             pipelineToChangeTo = 1;
         }
-        if (distance >= 20) {
+        if (distance >= 400) {
             pipelineToChangeTo = 2;
         }
 
+        // while (!validTarget) {
+        //     if (pipelineToChangeTo == 2) {
+        //         pipelineToChangeTo = 0;
+        //     }
+        //     pipelineToChangeTo++;
+        // }
+
+        // System.out.println(validTarget);
+        SmartDashboard.putNumber(getName() + "/Pipeline to Change to", pipelineToChangeTo);
+        // System.out.println(pipelineToChangeTo);
+
+        zoom = pipelineToChangeTo + 1;
+        // SmartDashboard.getNumber(getName() + "/Pipeline to Change to", efaultValue)
         limelightTable.getEntry("pipeline").setDouble(pipelineToChangeTo);
+
+        if (pipelineToChangeTo != 0)
+            pan = 1;
     }
 
     public void turnOnLEDs() {
