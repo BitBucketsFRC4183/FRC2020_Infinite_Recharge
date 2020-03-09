0f631bc161562080f2341d24ff00103a462ecbf8
Sajo8 <andrewsplin@protonmail.com>
2020-03-08 22:16:15 -0700

    work on the center-right auto trajectory

--- InfiniteRecharge/src/main/java/frc/robot/subsystem/drive/auto/FieldConstants.java
+++ InfiniteRecharge/src/main/java/frc/robot/subsystem/drive/auto/FieldConstants.java
@@ -4,32 +4,48 @@ import edu.wpi.first.wpilibj.geometry.Translation2d;
 
 // all units in m
 public class FieldConstants {
-    public static final double INITIALIZATION_LINE = 4.943475000000000;
+    public static final double INITIALIZATION_LINE = 5;
 
     // starting points
     public static final Translation2d START_CENTER_POWER_PORT = new Translation2d(
-        1.700784, INITIALIZATION_LINE
+        1.5, INITIALIZATION_LINE
     );
 
     public static final Translation2d START_OPPONENT_TRENCH = new Translation2d(
         -3.5, INITIALIZATION_LINE
     );
 
+    public static final Translation2d START_RIGHT_POWER_PORT = new Translation2d(
+        2.8, INITIALIZATION_LINE
+    );
+
     // power cell points
+
+    public static final Translation2d BEFORE_OUR_POWER_CELL_1 = new Translation2d(
+        3, 1.4
+    );
+    public static final Translation2d ALSO_BEFORE_OUR_POWER_CELL_1 = new Translation2d(
+        3.01, 1.41
+    );
+
     public static final Translation2d OUR_POWER_CELL_1 = new Translation2d(
-        3.400298, 1.828673
+        3, 1.83 
     );
 
     public static final Translation2d OUR_POWER_CELL_2 = new Translation2d(
-        3.400298, 0.914273
+        3, 0.91
     );
 
     public static final Translation2d OUR_POWER_CELL_3 = new Translation2d(
-        3.400298, -0.000127
+        3, -0.3
+    );
+
+    public static final Translation2d OUR_TWO_POWER_CELLS = new Translation2d(
+        3, -1.5
     );
 
     public static final Translation2d THEIR_TWO_POWER_CELLS = new Translation2d(
-        -3.5, 0.914273
+        -3.5, 0.75
     );
 
     /** Use the coordinate system the robot likes 
