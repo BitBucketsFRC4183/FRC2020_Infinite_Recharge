0b2ef896724045c601b1f11707df1b82ef866b35
Howard Beck <howard.beck314@gmail.com>
2020-03-30 22:10:08 -0700

    Adding noise covariances to KF and initial pose
    next step is to make it calculate stuff but that's scary so we're stopping here :)

--- InfiniteRecharge/src/main/java/frc/robot/subsystem/navigation/systems/RobotSystem1.java
+++ InfiniteRecharge/src/main/java/frc/robot/subsystem/navigation/systems/RobotSystem1.java
@@ -2,11 +2,20 @@ package frc.robot.subsystem.navigation.systems;
 
 import org.ejml.simple.SimpleMatrix;
 
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
 import frc.robot.config.Config;
 import frc.robot.subsystem.navigation.NavigationConstants;
+import frc.robot.utils.control.statespace.models.C2D;
 import frc.robot.utils.control.statespace.models.CStateSpaceModel;
+import frc.robot.utils.control.statespace.models.StateSpaceModel;
 import frc.robot.utils.control.statespace.observer.OutputObserver;
 import frc.robot.utils.control.statespace.system.StateSpaceSystem;
+import frc.robot.utils.data.filters.statespace.kalman.UnscentedKalmanFilter;
+import frc.robot.utils.data.noise.ConstantNoiseSource;
+import frc.robot.utils.data.noise.Noise;
+import frc.robot.utils.data.noise.NoiseSource;
+import frc.robot.utils.math.MathUtils;
 
 /*
  * ==================================
@@ -22,7 +31,7 @@ import frc.robot.utils.control.statespace.system.StateSpaceSystem;
  * also help if we're being pushed as then we can rely on the NavX IMU
  * and the LL to determine our position, without considering wheel encoders
  */
-public class RobotSystem1 extends StateSpaceSystem<CStateSpaceModel, OutputObserver> {
+public class RobotSystem1 extends StateSpaceSystem<StateSpaceModel, OutputObserver> {
     // states
     private static final int X_LENGTH = 6;
     private static final int X_X = 1; // x position of robot (m)
@@ -61,8 +70,7 @@ public class RobotSystem1 extends StateSpaceSystem<CStateSpaceModel, OutputObser
         errTol[X_VR] = ev;
         errTol[X_OFFSET] = etheta;
 
-
-        return new CStateSpaceModel(
+        CStateSpaceModel model =  new CStateSpaceModel(
             X_LENGTH,
             2, // volts to left and right motors
             NavigationConstants.KALMAN_FILTER_UPDATE_RATE_S,
@@ -99,10 +107,61 @@ public class RobotSystem1 extends StateSpaceSystem<CStateSpaceModel, OutputObser
                 xDot[X_OFFSET] = 0; // never changes (theoretically). In reality, IMU drifts a bit so it's okay if it changes
             }
         };
+
+        SimpleMatrix Qc = new SimpleMatrix(X_LENGTH, X_LENGTH);
+        Qc.insertIntoThis(X_VL, X_VL, config.drive.QVc_char);
+
+        model.addNoiseSource(new NoiseSource(X_LENGTH, Y_LENGTH) {
+            @Override
+            public Noise getNoise(SimpleMatrix x, SimpleMatrix u, double t, int k) {
+                double theta = x.get(X_THETA);
+                double vL = x.get(X_VL);
+                double vR = x.get(X_VR);
+                double v = (vL + vR) / 2;
+
+
+
+                // gotta linearize sometimes :(
+                SimpleMatrix A = new SimpleMatrix(X_LENGTH, X_LENGTH);
+
+                A.set(X_X, X_VL, Math.cos(theta) / 2);
+                A.set(X_X, X_VR, Math.cos(theta) / 2);
+                A.set(X_X, X_THETA, -v * Math.sin(theta));
+
+                A.set(X_Y, X_VL, Math.sin(theta) / 2);
+                A.set(X_Y, X_VR, Math.sin(theta) / 2);
+                A.set(X_Y, X_THETA, v * Math.cos(theta));
+
+                A.set(X_THETA, X_VL, -1 / (config.drive.trackWidth_in * NavigationConstants.IN_TO_M));
+                A.set(X_THETA, X_VR,  1 / (config.drive.trackWidth_in * NavigationConstants.IN_TO_M));
+
+                A.insertIntoThis(X_VL, X_VL, config.drive.Ac_char);
+
+
+
+                SimpleMatrix F = new SimpleMatrix(2*X_LENGTH, 2*X_LENGTH);
+                F.insertIntoThis(0, 0, A.negative());
+                F.insertIntoThis(X_LENGTH, X_LENGTH, A.transpose());
+                F.insertIntoThis(0, X_LENGTH, Qc);
+
+                // ??? why does SimpleMatrix have divide but not multiply lol
+                SimpleMatrix G = MathUtils.expm(F.divide(1.0 / NavigationConstants.KALMAN_FILTER_UPDATE_RATE_S));
+
+                SimpleMatrix S = C2D.getS(A, 0.02);
+                SimpleMatrix Ad = C2D.getA(S, X_LENGTH);
+
+                // get an approximation for the noise covariance
+                SimpleMatrix Q = Ad.mult(G.extractMatrix(0, X_LENGTH, X_LENGTH, 2*X_LENGTH));
+
+                return new Noise(Q);
+            }
+        });
+
+        return model;
     }
 
     public static OutputObserver generateObserver(Config config, RobotSystemConfig systemConfig) {
-        return new OutputObserver(6) {
+        OutputObserver observer = new OutputObserver(6) {
             @Override
             public SimpleMatrix getOutput(SimpleMatrix state, SimpleMatrix input, double t, int k) {
                 double theta = state.get(X_THETA);
@@ -139,14 +198,58 @@ public class RobotSystem1 extends StateSpaceSystem<CStateSpaceModel, OutputObser
                 return new SimpleMatrix(Y_LENGTH, 1, true, out);
             }
         };
+
+        SimpleMatrix R = new SimpleMatrix(Y_LENGTH, Y_LENGTH);
+        R.set(Y_TY, Y_TY, systemConfig.ll_angle_var);
+        R.set(Y_TX, Y_TX, systemConfig.ll_angle_var);
+        R.set(Y_YAW, Y_YAW, systemConfig.imu_yaw_var);
+
+        observer.addNoiseSource(new ConstantNoiseSource(Y_LENGTH, R));
+
+        return observer;
     }
 
 
+
+    private Config config;
+    private RobotSystemConfig systemConfig;
 
     public RobotSystem1(Config config, RobotSystemConfig systemConfig) {
         super(
             generateModel(config, systemConfig),
             generateObserver(config, systemConfig)
         );
+
+        this.config = config;
+        this.systemConfig = systemConfig;
+
+        setFilter(createFilter());
+    }
+
+
+
+    private UnscentedKalmanFilter createFilter() {
+        SimpleMatrix P0 = new SimpleMatrix(X_LENGTH, X_LENGTH);
+        P0.set(X_X, X_X, systemConfig.x0_var);
+        P0.set(X_Y, X_Y, systemConfig.y0_var);
+        P0.set(X_THETA, X_THETA, systemConfig.theta_var);
+        P0.set(X_THETA, X_OFFSET, systemConfig.theta_var);
+        P0.set(X_OFFSET, X_THETA, systemConfig.theta_var);
+        P0.set(X_OFFSET, X_OFFSET, systemConfig.theta_var);
+        P0.set(X_VL, X_VL, systemConfig.velocity_var);
+        P0.set(X_VR, X_VR, systemConfig.velocity_var);
+
+        // we love Java
+        return new UnscentedKalmanFilter(this, P0);
+    }
+
+    public void setInitialPose(Pose2d pose) {
+        SimpleMatrix x0 = new SimpleMatrix(X_LENGTH, 1);
+
+        Translation2d translation = pose.getTranslation();
+        // inverse of FieldConstants.transformToRobot
+        x0.set(X_X, 0, translation.getY());
+        x0.set(X_Y, 0, -translation.getX());
+        x0.set(X_THETA, pose.getRotation().getRadians());
     }
 }
