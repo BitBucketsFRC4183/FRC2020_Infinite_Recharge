38b54bad4d46f5fa7f4b957cf9391322c91c6810
Ojas Sanghi <ojas.sanghi@protonmail.com>
2020-06-13 20:01:32 -0700

    fix issues

--- InfiniteRecharge/src/main/java/frc/robot/subsystem/drive/auto/AutoConfig.java
+++ InfiniteRecharge/src/main/java/frc/robot/subsystem/drive/auto/AutoConfig.java
@@ -1,11 +1,11 @@
 package frc.robot.subsystem.drive.auto;
 
 public class AutoConfig {
-    public static final double cruiseSpeed_mps = 0.3 * 14;
-    public static final double maxAcceleration_mps = 2;
+    public static double cruiseSpeed_mps = 0.3 * 14;
+    public static double maxAcceleration_mps = 2;
 
-    public static final double b = 2;
-    public static final double zeta = 0.35;
+    public static double b = 2;
+    public static double zeta = 0.35;
 
-    public static final double kP = 0.911;
+    public static double kP = 0.911;
 }
--- InfiniteRecharge/src/main/java/frc/robot/subsystem/scoring/intake/IntakeConfig.java
+++ InfiniteRecharge/src/main/java/frc/robot/subsystem/scoring/intake/IntakeConfig.java
@@ -1,11 +1,14 @@
 package frc.robot.subsystem.scoring.intake;
 
+import frc.robot.config.MotorConfig;
+
 public class IntakeConfig {
 
     public static final double INTAKE_OUTPUT = 0.5;
     public static final double OUTAKE_OUTPUT = 0.5;
 
-    public static final int INTAKE_MOTOR_ID = 10;
-    
     public static boolean intakePivotEnabled = true;
+
+    public MotorConfig intake = new MotorConfig();
+
 }
